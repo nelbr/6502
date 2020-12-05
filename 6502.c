900fe28b91f8132472bf91e0e4b6c3b040b35fb8
nelbr <nwaissman@yahoo.com.br>
2020-12-05 15:44:30 +0100

    remove DEBUG flag

--- 6502.c
+++ 6502.c
@@ -40,34 +40,6 @@
 #define INDIRECT 12
 #define RELATIVE 13
 
-/*
- 
-#define STATUS_TO_BINARY_PATTERN "     Ne %c Ov %c NA %c Br %c De %c In %c Ze %c Ca %c\n"
-#define STATUS_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0')
-
-typedef struct microprocessor {
-	unsigned char a;
-	unsigned char x;
-	unsigned char y;
-    unsigned char sp;
-    unsigned short pc;
-	unsigned char status;
-    unsigned long cycles;
-} microprocessor;
-
-microprocessor cpu;
-unsigned char used; 
-
-*/
-
 // 
 // Read the next opcode from current pc value
 //
--- 6502.h
+++ 6502.h
@@ -12,10 +12,11 @@
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+//    along with lib6502.  If not, see <https://www.gnu.org/licenses/>.
 //
-// Nelson Luiz Waissman - Summer 2020
+//    nelbr - Summer 2020
 //
+
 #ifndef MOS_H
 #define MOS_H
 
